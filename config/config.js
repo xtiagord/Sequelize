b6eb8cbd3f730654fc30e4d6fb11a26d4a4727e1
Tiago Dalmolin <tiagodalmolin028@gmail.com>
2023-04-09 22:48:26 -0300

    Sequelize

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "memesdanet2012",
+    database: "curso_sequelize",
+    define: {
+      timestamps: true,
+    },
+  };
